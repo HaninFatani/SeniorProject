2b8be859acfd640ce3d8d5c042fbeb26cb455feb
HaninFatani <norma.chan94@gmail.com>
2016-11-27 12:51:38 +0300

    Update InstructorLL.java

--- InstructorLL.java
+++ InstructorLL.java
@@ -1,140 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package algorithm;
 
 /**
  *
  * @author Haninft
  */
-public class InstructorLL {
-    instrutorInfo head;
+public class instrutorInfo {
+    //variables I need 
+    instrutorInfo next;
+    private String ID;
+    private double workload , effort;
+    private int maxWorkload, experience;
+    private String majorI;
+    private String request [];
+    
+    
+    public instrutorInfo() {
+        this.next = null;
+        this.ID = null;
 
-    public InstructorLL() {
-        this.head = null;
     }
-     
-    public boolean isEmpty(){
-        return this.head == null;
-    }
-    public boolean search(String data){
-        return search(this.head, data);
-    }
-    private boolean search(instrutorInfo p, String data){
-       
+
+    public instrutorInfo(instrutorInfo next, String ID, double workload, double effort, int maxWorkload, String majorI, String request, int experience) {
+        this.next = next;
+        this.ID = ID;
+        this.workload = workload;
+        this.effort = effort;
+        this.maxWorkload = maxWorkload;
+        this.majorI = majorI;
+        this.request = new String[1];
+        for(int i = 0 ; i < this.request.length ; i++){
+            if(this.request[i] == null){
+                this.request[i] = request;
+                break;
+            }
+        }
         
-        while (p!= null) {
-            if (p.getID().equals(data)) {  //If data is found
+        this.experience = experience;
+        
+    }
+
+    public instrutorInfo getNext() {
+        return next;
+    }
+
+    public void setNext(instrutorInfo next) {
+        this.next = next;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(double workload) {
+        this.workload = workload;
+    }
+
+    public double getEffort() {
+        return effort;
+    }
+
+    public void setEffort(double effort) {
+        this.effort = effort;
+    }
+
+    public int getMaxWorkload() {
+        return maxWorkload;
+    }
+
+    public void setMaxWorkload(int maxWorkload) {
+        this.maxWorkload = maxWorkload;
+    }
+
+    public String getMajorI() {
+        return majorI;
+    }
+
+    public void setMajorI(String majorI) {
+        this.majorI = majorI;
+    }
+
+    public boolean getRequest(String match) {
+        for(int i = 0 ; i < this.request.length ; i++){
+            if(this.request[i].equals(match)){
+               
                 return true;
             }
-            p = p.getNext();  //Goes to next node in the list
         }
-        return false;  //If data is not found after traversing the entire link list
+        return false;
     }
-    
-    //=================================================
-    public void insert(String id, double workload, double effort, int maxWorkload, String majorI, String request ) { 
-        
-        this.head = insert(this.head, id, workload, effort, maxWorkload, majorI, request);
-    }
-    
-    private instrutorInfo insert(instrutorInfo next, String id, double workload, double effort, int maxWorkload, String majorI, String request) { 
-       
-        
-        if (head == null || Integer.parseInt(head.getID().substring(1,2)) < Integer.parseInt(id)) { //if the head is empty or the value of the head is smaller than the value we recieved
-            // we will sort th values according to the student's id in decreasing order
-            head = new instrutorInfo(head, id, workload, effort, maxWorkload, majorI, request);  
-                                                                  
-            return head;
-        } 
 
-        else {
-           
-            instrutorInfo helpPtr = head;
-
-            while (helpPtr.getNext() != null) { 
-                
-                if (Integer.parseInt(head.getNext().getID().substring(1,2)) < Integer.parseInt(id)) {//if the help-pointer is empty or the value of the head is smaller than the value we recieved
-                                                     // we will sort th values according to the student's id in decreasing order
-                    break; 
-                }
-                //move to the next node
-                helpPtr = helpPtr.getNext();
+    public void setRequest(String request) {
+        for(int i = 0 ; i < this.request.length ; i++){
+            if(this.request[i] == null){
+                this.request[i] = request;
+                break;
             }
-            instrutorInfo newNode = new instrutorInfo(helpPtr.getNext(), id, workload, effort, maxWorkload, majorI, request);
-            helpPtr.setNext(newNode); 
         }
-        // Return head
-        return head;
-    }
-    
-    //======================================================================
-    // Displaying a specific node
-     public String display(String data) { 
-        return display(head, data); 
-                                                       
     }
 
-    private String display(instrutorInfo p, String data) { 
-        
-        instrutorInfo helpPtr = p;  
-        
-        while (helpPtr != null) {   
-        
-            if (helpPtr.getID().equals(data)) {
-               
-              
-              
-            }
-            helpPtr = helpPtr.getNext(); 
-        }
-        return null;
-    }
-    
-      
-    //======================================================================
-    //return instructor id
-     public String returnInstructorID(String data) { 
-        return returnInstructorID(head, data); 
-                                                       
+    public int getExperience() {
+        return experience;
     }
 
-    private String returnInstructorID(instrutorInfo p, String data) { 
-        
-        instrutorInfo helpPtr = p;  
-        
-        while (helpPtr != null) {   
-        
-            if (helpPtr.getID().equals(data)) {
-               
-                return helpPtr.getID();
-              
-            }
-            helpPtr = helpPtr.getNext(); 
-        }
-        return null;
+    public void setExperience(int experience) {
+        this.experience = experience;
     }
     
-    public String[][] returnRequest(String data){
-        return returnRequest(this.head, data);
-    }
-    
-    private String[][] returnRequest(instrutorInfo p, String data){
-       
-        String listInstructor[][] = new String[10][5];
-        int i = 0;
-        while (p!= null) {
-            if (p.getRequest(data) == true) {  //If data is found
-                
-                listInstructor[i][0] = p.getID();
-                listInstructor[i][1] = ""+p.getWorkload();
-                listInstructor[i][2] = ""+p.getEffort();
-                listInstructor[i][3] = ""+p.getMaxWorkload();
-                listInstructor[i][4] = p.getMajorI();
-                i++;
-            }
-            p = p.getNext();  //Goes to next node in the list
-        }
-        return listInstructor;  //If data is not found after traversing the entire link list
-    }
-
-
     
 }
